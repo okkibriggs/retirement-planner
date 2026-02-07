c5f4a926a6153120ade9ede1d8ad2d7688095191
Harrison Krowas <hkrowas@macbookair.lan>
2026-02-07 15:50:44 -0500

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-envsubst '${PORT}' < /app/nginx.conf.template > /etc/nginx/conf.d/default.conf
-streamlit run app.py --server.port 8501 --server.headless true &
+export PORT=${PORT:-8080}
+export STREAMLIT_SERVER_PORT=8501
+
+envsubst '${PORT}' < /app/nginx.conf.template > /etc/nginx/conf.d/app.conf
+rm -f /etc/nginx/sites-enabled/default
+
+nginx -t && echo "nginx config OK" || echo "nginx config FAILED"
+
+streamlit run app.py --server.headless true &
+sleep 2
 nginx -g 'daemon off;'
